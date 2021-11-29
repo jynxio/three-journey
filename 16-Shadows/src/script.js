dbc1c08d4475ffc76fd2a1385ce23e1eaba4bc5a
Matrix <1337816495@qq.com>
2021-11-29 14:17:37 +0800

    Update script.js

--- 16-Shadows/src/script.js
+++ 16-Shadows/src/script.js
@@ -17,18 +17,60 @@ const scene = new three.Scene();
 
 
 // Light
-const ambient_light = new three.AmbientLight(0xffffff, 0.5);
+const ambient_light = new three.AmbientLight(0xffffff, 0.4);
 scene.add(ambient_light);
 gui.add(ambient_light, "intensity").min(0).max(1).step(0.001);
 
-const directional_light = new three.DirectionalLight(0xffffff, 0.5);
+const directional_light = new three.DirectionalLight(0xffffff, 0.4);
 directional_light.position.set(2, 2, - 1);
+directional_light.castShadow = true;
+directional_light.shadow.mapSize.width = 1024;
+directional_light.shadow.mapSize.height = 1024;
+directional_light.shadow.camera.near = 1;
+directional_light.shadow.camera.far = 6;
+directional_light.shadow.camera.top = 2;
+directional_light.shadow.camera.right = 2;
+directional_light.shadow.camera.bottom = -2;
+directional_light.shadow.camera.left = -2;
+directional_light.shadow.radius = 10;
 scene.add(directional_light)
+
+const directional_light_camera_helper = new three.CameraHelper(directional_light.shadow.camera);
+directional_light_camera_helper.visible = false;
+scene.add(directional_light_camera_helper);
+
 gui.add(directional_light, "intensity").min(0).max(1).step(0.001);
 gui.add(directional_light.position, "x").min(- 5).max(5).step(0.001);
 gui.add(directional_light.position, "y").min(- 5).max(5).step(0.001);
 gui.add(directional_light.position, "z").min(- 5).max(5).step(0.001);
 
+const spot_light = new three.SpotLight(0xffffff, 0.4, 10, Math.PI * 0.3);
+spot_light.castShadow = true;
+spot_light.position.set(0, 2, 2);
+spot_light.shadow.mapSize.width = 1024;
+spot_light.shadow.mapSize.height = 1024;
+spot_light.shadow.camera.fov = 30;
+spot_light.shadow.camera.near = 1;
+spot_light.shadow.camera.far = 6;
+// spot_light.shadow.radius = 10;
+scene.add(spot_light);
+scene.add(spot_light.target);
+
+// const spot_light_camera_helper = new three.CameraHelper(spot_light.shadow.camera);
+// scene.add(spot_light_camera_helper);
+
+// const spot_light_helper = new three.SpotLightHelper(spot_light);
+// scene.add(spot_light_helper);
+// spot_light_helper.update();
+
+const point_light = new three.PointLight(0xffffff, 0.3);
+point_light.castShadow = true;
+point_light.position.set(-1, 1, 0);
+scene.add(point_light);
+
+const point_light_camera_helper = new three.CameraHelper(point_light.shadow.camera);
+scene.add(point_light_camera_helper);
+// TODO 从 PointLight 开始
 
 // Materials
 const material = new three.MeshStandardMaterial();
@@ -42,6 +84,7 @@ const sphere = new three.Mesh(
     new three.SphereGeometry(0.5, 32, 32),
     material
 );
+sphere.castShadow = true;
 
 const plane = new three.Mesh(
     new three.PlaneGeometry(5, 5),
@@ -49,8 +92,8 @@ const plane = new three.Mesh(
 );
 plane.rotation.x = - Math.PI * 0.5;
 plane.position.y = - 0.5;
+plane.receiveShadow = true;
 scene.add(sphere, plane);
-
 
 // Size
 const size = {
@@ -91,6 +134,9 @@ const renderer = new three.WebGLRenderer({
 });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = three.BasicShadowMap;
+// renderer.shadowMap.type = three.PCFSoftShadowMap;
 
 
 // Animate
