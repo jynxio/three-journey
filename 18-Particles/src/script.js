2a8a4b5f06dfabbc5dc095f391c2d4a5abab32ac
Matrix <1337816495@qq.com>
2021-12-02 09:14:59 +0800

    add 18-Particles

---
+++ 18-Particles/src/script.js
@@ -0,0 +1,96 @@
+import "./style.css";
+import * as three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new three.Scene();
+
+
+/**
+ * Textures
+ */
+const texture_loader = new three.TextureLoader();
+
+
+/**
+ * Test cube
+ */
+const cube = new three.Mesh(
+    new three.BoxGeometry(1, 1, 1),
+    new three.MeshBasicMaterial()
+);
+scene.add(cube);
+
+
+/**
+ * Sizes
+ */
+const size = {
+    width: window.innerWidth,
+    height: window.innerHeight
+};
+
+window.addEventListener("resize", () => {
+
+    size.width = window.innerWidth;
+    size.height = window.innerHeight;
+
+    camera.aspect = size.width / size.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(size.width, size.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new three.PerspectiveCamera(75, size.width / size.height, 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+
+/**
+ * Renderer
+ */
+const renderer = new three.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+
+/**
+ * Animate
+ */
+const clock = new three.Clock();
+
+tick();
+
+function tick() {
+
+    window.requestAnimationFrame(tick);
+
+    const elapsed_time = clock.getElapsedTime();
+
+    controls.update();
+    renderer.render(scene, camera);
+
+}
