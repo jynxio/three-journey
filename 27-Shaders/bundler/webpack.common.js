929c9b87c76808e5c479d2518d65e547d6bd96ea
Matrix <1337816495@qq.com>
2022-02-20 22:42:03 +0800

    u

--- 27-Shaders/bundler/webpack.common.js
+++ 27-Shaders/bundler/webpack.common.js
@@ -21,6 +21,13 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
             },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/i,
+                type: "asset/source",
+                generator: {
+                    filename: "assets/images/[hash][ext]",
+                }
+            }
         ],
     },
     output: {
--- 27-Shaders/source/index.js
+++ 27-Shaders/source/index.js
@@ -70,24 +70,8 @@ renderer.setAnimationLoop( function loop() {
 /* Test Mesh */
 const geometry = new three.PlaneGeometry( 1, 1, 32, 32 );
 const material = new three.RawShaderMaterial( {
-    vertexShader: `
-        uniform mat4 projectionMatrix;
-        uniform mat4 viewMatrix;
-        uniform mat4 modelMatrix;
-
-        attribute vec3 position;
-
-        void main() {
-            gl_Position = projectionMatrix * viewMatrix * vec4( position, 1.0 );
-        }
-    `,
-    fragmentShader: `
-        precision mediump float;
-
-        void main() {
-            gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );
-        }
-    `,
+    vertexShader: vertex_shader,
+    fragmentShader: fragment_shader,
 } );
 const mesh = new three.Mesh( geometry, material );
 
