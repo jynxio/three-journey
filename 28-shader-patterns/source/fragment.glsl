6f291eeca262c994982bbf6915241f18e5639355
Matrix <1337816495@qq.com>
2022-04-05 00:56:50 +0800

    u

--- 28-shader-patterns/source/fragment.glsl
+++ 28-shader-patterns/source/fragment.glsl
@@ -2,11 +2,15 @@ precision mediump float;
 
 varying vec2 vUv;
 
+float random( vec2 st ) {
+
+    return fract( sin( dot( st.xy, vec2( 12.9898, 78.233 ) ) ) * 43758.5453123 );
+
+}
+
 void main() {
 
-    float strength = mod( vUv.x * 10.0, 1.0 );
-
-    strength = step( 0.8, strength );
+    float strength = 0.015 / distance( vUv, vec2( 0.5 ) );
 
     gl_FragColor = vec4( vec3( strength ), 1.0 );
 
