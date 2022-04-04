ad4bf60ec3fcc71b0c1af5f5854d78d608be29de
Matrix <1337816495@qq.com>
2022-04-04 23:00:29 +0800

    u

---
+++ 28-shader-patterns/source/index.js
@@ -0,0 +1,69 @@
+import "/style/reset.css";
+
+import "/style/index.css";
+
+import * as three from "three";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import Gui, { GUI } from "lil-gui";
+
+import test_vertex_shader from "./vertex.glsl";
+
+import test_fragment_shader from "./fragment.glsl";
+
+/* ------------------------------------------------------------------------------------------------------ */
+/* Renderer */
+const renderer = new three.WebGLRenderer( { antialias: window.devicePixelRatio < 2 } );
+
+renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+document.body.append( renderer.domElement );
+
+/* Scene */
+const scene = new three.Scene();
+
+/* Camera */
+const camera = new three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 100 );
+
+camera.position.set(0.25, - 0.25, 1)
+scene.add( camera );
+
+/* Controls */
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.enableDamping = true;
+
+/* Resize */
+window.addEventListener( "resize", _ => {
+
+    renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
+    renderer.setSize( window.innerWidth, window.innerHeight);
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+} );
+
+/* My code */
+const geometry = new three.PlaneGeometry( 1, 1, 32, 32 );
+const material = new three.RawShaderMaterial( {
+    vertexShader: test_vertex_shader,
+    fragmentShader: test_fragment_shader,
+    wireframe: false,
+    side: three.DoubleSide,
+} );
+const mesh = new three.Mesh( geometry, material );
+
+scene.add( mesh );
+
+/* Render */
+const clock = new three.Clock();
+
+renderer.setAnimationLoop( function loop() {
+
+    controls.update();
+    renderer.render( scene, camera );
+
+} );
